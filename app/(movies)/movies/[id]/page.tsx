df6195ed195b2eb648057408a17bd09bed977aea
Dong il Kwon <30903150+dikwon79@users.noreply.github.com>
2024-11-07 02:07:43 +0900

    error solved

--- app/(movies)/movies/[id]/page.tsx
+++ app/(movies)/movies/[id]/page.tsx
@@ -3,18 +3,19 @@ import MovieInfo, { getMovie } from "../../../../components/movie-info";
 import MovieVidoes from "../../../../components/movie-videos";
 
 interface IParams{ 
-    params: {id: string};
+    params: Promise<{id: string}>;
 }
 
-export async function generateMetadata({params: {id}}:IParams){
+export async function generateMetadata({params}:IParams){
+    const { id } = await params;
     const movie = await getMovie(id);
     return {
         title: movie.title,
     }
 }
-export default async function MovieDetail( {params: {id}}: IParams) {
+export default async function MovieDetail( {params}: IParams) {
     
-  
+    const { id } = await params;
     return (
         <div>
             <Suspense fallback ={<h1>Loading movie info</h1>}>
